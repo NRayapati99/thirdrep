faa7ec768ef84700acc857e7465b4cc244b4e80c
NRayapati99 <rayapatinaveen88@gmail.com>
2025-01-28 16:56:20 -0600

    a

---
+++ code.py
@@ -0,0 +1,6 @@
+number1 = int(input("give the first numbers: "))
+numbers2 = int(input("give the second umbers: "))
+
+sum_result = number1+numbers2
+
+print(f"the sum of {number1} and {numbers2} is {sum_result}.")
